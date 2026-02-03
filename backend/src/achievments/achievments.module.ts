32732785c892460fa317deba3c7a189d84f7e33b
yoanastamenova <yoana.stamenovaa@gmail.com>
2026-02-03 11:41:08 +0100

    feat: register the entities to their modules

--- backend/src/achievments/achievments.module.ts
+++ backend/src/achievments/achievments.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Achievments } from './entities/achievments.entity';
+import { UserAchievments } from './entities/user-achievment.entity';
 import { AchievmentsService } from './achievments.service';
 import { AchievmentsController } from './achievments.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Achievments, UserAchievments])],
   controllers: [AchievmentsController],
   providers: [AchievmentsService],
 })
