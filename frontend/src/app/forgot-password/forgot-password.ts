452c60be6d901f78b6e24d2ef7c0a3f3b80f0520
yoanastamenova <yoana.stamenovaa@gmail.com>
2026-02-25 19:56:38 +0100

    feat: complete finish password view

--- frontend/src/app/forgot-password/forgot-password.ts
+++ frontend/src/app/forgot-password/forgot-password.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './forgot-password.html',
   styleUrl: './forgot-password.css',
 })
