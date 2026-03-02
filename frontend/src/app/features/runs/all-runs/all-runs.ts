6a5982e3855feeab4a26d2076afaad00cec99599
yoanastamenova <yoana.stamenovaa@gmail.com>
2026-03-02 14:33:16 +0100

    feat: complete all runs view

--- frontend/src/app/features/runs/all-runs/all-runs.ts
+++ frontend/src/app/features/runs/all-runs/all-runs.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RunsService } from '../../../core/services/runs-service';
+import { RunInterface } from '../../../interfaces/run.interface';
+import { Navbar } from '../../../shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-all-runs',
-  imports: [],
+  imports: [Navbar],
   templateUrl: './all-runs.html',
   styleUrl: './all-runs.css',
 })
-export class AllRuns {
+export class AllRuns implements OnInit {
+  runs: RunInterface[] = [];
+
   constructor(private runService: RunsService) {}
+
+  ngOnInit() {
+    this.runService.getRuns().subscribe({
+      next: (data) => {
+        this.runs = data;
+      },
+      error: (err) => {
+        console.error('Failed to load runs:', err);
+      },
+    });
+  }
+
+  formatDuration(seconds: number): string {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${mins}:${secs.toString().padStart(2, '0')}`;
+  }
+
+  formatDate(date: Date): string {
+    return new Date(date).toLocaleDateString('en-US', {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+    });
+  }
 }
