e59e452dba2dac3c790a8bf084c4d034804b2255
yoanastamenova <yoana.stamenovaa@gmail.com>
2026-03-06 12:01:48 +0100

    feat: add spinner dots with animation for loading state in login, register, runs etc

--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -3,14 +3,17 @@ import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AlertCard } from '../../../shared/components/alert-card/alert-card';
 import { AuthService } from '../../../core/services/auth-service';
+import { LoadingDots } from '../../../shared/components/loading-dots/loading-dots';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, FormsModule, AlertCard],
+  imports: [RouterLink, FormsModule, AlertCard, LoadingDots],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  isLoading = false;
+
   // Form data
   email = '';
   password = '';
@@ -34,6 +37,8 @@ export class Login {
       return;
     }
 
+    this.isLoading = true;
+
     this.authService.login(this.email, this.password).subscribe({
       next: (response: any) => {
         this.authService.saveToken(response.access_token);
@@ -41,6 +46,7 @@ export class Login {
         this.router.navigate(['/profile']);
       },
       error: () => {
+        this.isLoading = false;
         this.loginError = 'Invalid email or password.';
       },
     });
