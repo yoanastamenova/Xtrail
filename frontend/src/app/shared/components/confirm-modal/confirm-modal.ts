0bac224d446ce727696280bc9e1eb000326f3c18
yoanastamenova <yoana.stamenovaa@gmail.com>
2026-03-09 13:13:23 +0100

    feat: add component modal for confirm delete or change dialog window

---
+++ frontend/src/app/shared/components/confirm-modal/confirm-modal.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-modal',
+  imports: [],
+  templateUrl: './confirm-modal.html',
+  styleUrl: './confirm-modal.css',
+})
+export class ConfirmModal {
+  @Input() title = 'Confirm';
+  @Input() message = 'Are you sure?';
+  @Input() confirmText = 'Delete';
+  @Input() cancelText = 'Cancel';
+
+  @Output() confirmed = new EventEmitter<void>();
+  @Output() cancelled = new EventEmitter<void>();
+
+  onConfirm() {
+    this.confirmed.emit();
+  }
+
+  onCancel() {
+    this.cancelled.emit();
+  }
+}
